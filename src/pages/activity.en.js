625a67a35862de4c61d95c0df58a6624627a2751
KANDA, Tetsuya <t-kanda@ist.osaka-u.ac.jp>
2021-04-01 17:40:08 +0900

    activity

--- src/pages/activity.en.js
+++ src/pages/activity.en.js
@@ -1,21 +1,26 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import propTypes from "../components/defaultPropTypes"
+import propTypes from "../components/defaultPropTypes";
 
-import ServiceMDX from "../data/activity/service.en.mdx"
-import AwardsMDX from "../data/activity/awards.en.mdx"
+import ServiceMDX from "../data/activity/service.en.mdx";
+import AwardsMDX from "../data/activity/awards.en.mdx";
+import VisitMDX from "../data/activity/visit.en.mdx";
+import StudentMDX from "../data/activity/student.en.mdx";
 
-const ServicePage = ({location}) => (
+const ServicePage = ({ location }) => (
   <Layout location={location}>
-    <SEO title="Service" />
+    <SEO title="Activity" />
+    <h1>Activity</h1>
     <ServiceMDX />
     <AwardsMDX />
+    <VisitMDX />
+    <StudentMDX />
   </Layout>
-)
+);
 
 ServicePage.propTypes = propTypes;
 
-export default ServicePage
+export default ServicePage;
--- src/pages/activity.ja.js
+++ src/pages/activity.ja.js
@@ -1,21 +1,26 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import propTypes from "../components/defaultPropTypes"
+import propTypes from "../components/defaultPropTypes";
 
-import ServiceMDX from "../data/activity/service.ja.mdx"
-import AwardsMDX from "../data/activity/awards.ja.mdx"
+import ServiceMDX from "../data/activity/service.ja.mdx";
+import AwardsMDX from "../data/activity/awards.ja.mdx";
+import VisitMDX from "../data/activity/visit.ja.mdx";
+import StudentMDX from "../data/activity/student.ja.mdx";
 
-const ServicePage = ({location}) => (
+const ServicePage = ({ location }) => (
   <Layout location={location}>
-    <SEO title="Service" />
+    <SEO title="各種活動" />
+    <h1>かつどう：各種活動</h1>
     <ServiceMDX />
     <AwardsMDX />
+    <VisitMDX />
+    <StudentMDX />
   </Layout>
-)
+);
 
 ServicePage.propTypes = propTypes;
 
-export default ServicePage
+export default ServicePage;
