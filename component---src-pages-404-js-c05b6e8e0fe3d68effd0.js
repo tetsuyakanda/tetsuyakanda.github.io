e6207b1a1e4b39b95138c3c14c65733efbb82a8d
KANDA, Tetsuya <kanda@m.ndsu.ac.jp>
2026-07-20 07:16:03 +0000

    Updates

---
+++ component---src-pages-404-js-c05b6e8e0fe3d68effd0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_tkweb=self.webpackChunkgatsby_tkweb||[]).push([[125],{3331:function(e,t,n){n.r(t),n.d(t,{Head:function(){return l}});var a=n(6540),u=n(4655),c=n(7528);function l(){return a.createElement(c.A,{title:"404: Not found"})}t.default=e=>a.createElement(u.A,{location:e.location},a.createElement("h1",null,"NOT FOUND"))}}]);
+//# sourceMappingURL=component---src-pages-404-js-c05b6e8e0fe3d68effd0.js.map
